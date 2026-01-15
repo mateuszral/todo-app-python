8e0c3f78c8fbc2cd420a7cc203777add66b875ee
mateusz_ral <mral16102000@gmail.com>
2026-01-15 17:51:19 +0100

    add adding tasks basic logic and change deadline

--- main.py
+++ main.py
@@ -10,9 +10,8 @@ def print_tasks(tasks):
     print("\nYour tasks:")
     for index, task in enumerate(tasks):
         print(
-            f"{index + 1}. {task['description']} {'✅' if task['is_completed'] else ''} {task.get('deadline', '')}"
+            f"{index + 1}. {task['description']} {task["deadline"]} {'✅' if task['is_completed'] else ''}"
         )
-    print()
 
 
 # boilerplate
@@ -26,7 +25,8 @@ tasks = [
     {
         "id": 2, 
         "description": "Walk the dog", 
-        "is_completed": True
+        "is_completed": True,
+        "deadline": "",
     },
     {
         "id": 3,
@@ -44,12 +44,24 @@ while choice != 4:
     match choice:
         case 1:
             print_tasks(tasks)
-            print()
-            print_menu()
         case 2:
-            print_tasks(tasks)
+            new_task_description = input("Enter task name: ")
+            new_task_deadline = input("Enter deadline (optional) [DD-MM-YYYY]: ")
+            
+            tasks.append(
+                {
+                    "id": len(tasks) + 1,
+                    "description": new_task_description.capitalize(),
+                    "is_completed": False,
+                    "deadline": new_task_deadline,
+                }
+            )
+            
+            print("\nTask added successfully")
         case 3:
             print("delete")
+    print()
+    print_menu()
     choice = int(input("Enter your choice: "))
 
 exit()
